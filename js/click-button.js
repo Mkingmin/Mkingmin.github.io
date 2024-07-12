c045b60d6eaa79ec896d36c64286056a1a91dd45
Nguyen Vo Anh Minh <125110267+Mkingmin@users.noreply.github.com>
2024-07-12 22:54:44 +0700

    Add files via upload

---
+++ js/click-button.js
@@ -0,0 +1,9 @@
+
+function toggleSection() {
+  var image = document.getElementById("skillset-img");
+  if (image.style.display === "none"){
+    image.style.display = "block"; // Show the image
+    } else {
+      image.style.display = "none"; // Hide the image
+  }
+}
